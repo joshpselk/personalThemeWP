5c7e8c20a73b683b166372059bb5e393ea82a3ba
joshpselk <joshselk@hotmail.com>
2016-05-30 19:07:26 -0500

    Adds portfolio gallery to page

--- index.php
+++ index.php
@@ -2,14 +2,12 @@
 <div id="main">
 	<div class="row">
 		<div id="content" class="large-12 medium-12 small-12">
-			<h1>Main Area</h1>
-			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-				<h1><?php the_title(); ?></h1>
-				<h4>Posted on <?php the_time('F jS, Y') ?></h4>
-				<p><?php the_content(__('(more...)')); ?></p>
-			<hr> <?php endwhile; else: ?>
-				<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-			<?php endif; ?>
+			<?php if( get_field( 'gallery_shortcode', 'options' ) ) { 
+				$portfolioCode = get_field( 'gallery_shortcode', 'options' ); ?>
+				<div>
+					<?php echo do_shortcode( $portfolioCode ); ?>
+				</div>
+			<?php } ?>
 		</div><!--content-->
 	</div><!--row-->
 	<?php get_sidebar(); ?>
